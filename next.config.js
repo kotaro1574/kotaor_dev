5a19bcb4d358ee25edc1015c4b205deed71abe2a
kotaro1574 <shake1574@gmail.com>
2022-05-21 17:17:48 +0900

    update: next.config.js設定

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: ['page.tsx', 'api.ts'],
+  rewrites: async () => [
+    {
+      destination: '/home',
+      source: '/',
+    },
+  ],
 }
 
 module.exports = nextConfig
