6b644c9ad2446bfa295372edc10f17e87f169f75
kotaro1574 <shake1574@gmail.com>
2022-11-03 15:49:54 +0900

    create: env.ts作成

---
+++ src/constant/env.ts
@@ -0,0 +1,6 @@
+export const AUTH0_DOMAIN = process.env['NEXT_PUBLIC_AUTH0_DOMAIN'] ?? ''
+
+export const AUTH0_CLIENT_ID = process.env['NEXT_PUBLIC_AUTH0_CLIENT_ID'] ?? ''
+
+export const REDIRECT_URI =
+  process.env['NEXT_PUBLIC_AUTH0_REDIRECT_URL'] ?? 'http://localhost:3000'
