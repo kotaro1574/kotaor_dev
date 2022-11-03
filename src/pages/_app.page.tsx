17aa4112eff5e47c602b62695947acd13659ef71
kotaro1574 <shake1574@gmail.com>
2022-11-03 15:50:48 +0900

    update: _app.page.tsxにAuth0Providerを定義

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,12 +1,20 @@
 import type { AppPropsWithLayout } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import { Auth0Provider } from '@auth0/auth0-react'
+import { AUTH0_CLIENT_ID, AUTH0_DOMAIN, REDIRECT_URI } from '@src/constant/env'
 
 const App = ({ Component, pageProps }: AppPropsWithLayout) => {
   const getLayout = Component.getLayout ?? ((page) => page)
   return (
-    <ChakraProvider>
-      {getLayout(<Component {...pageProps} />, pageProps)}
-    </ChakraProvider>
+    <Auth0Provider
+      domain={AUTH0_DOMAIN}
+      clientId={AUTH0_CLIENT_ID}
+      redirectUri={REDIRECT_URI}
+    >
+      <ChakraProvider>
+        {getLayout(<Component {...pageProps} />, pageProps)}
+      </ChakraProvider>
+    </Auth0Provider>
   )
 }
 
