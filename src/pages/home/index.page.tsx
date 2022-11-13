57c410ecd99bab705879fb3559d64cfa8c0d9b43
kotaro1574 <shake1574@gmail.com>
2022-11-13 16:22:37 +0900

    update: SSRでtoken取得してみたお

--- src/pages/home/index.page.tsx
+++ src/pages/home/index.page.tsx
@@ -1,6 +1,7 @@
-import { NextPageWithLayout } from 'next'
+import { GetServerSideProps, NextPageWithLayout } from 'next'
 import { Container } from '@chakra-ui/react'
 import { PageContent } from '@src/pages/home/index.content'
+import { getAccessToken } from '@auth0/nextjs-auth0'
 
 const Home: NextPageWithLayout = () => {
   return (
@@ -17,6 +18,15 @@ const Home: NextPageWithLayout = () => {
  */
 Home.getLayout = (page, props) => {
   return <>{page}</>
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { req, res } = context
+  const accessToken = await getAccessToken(req, res)
+  console.log(accessToken)
+  return {
+    props: {},
+  }
 }
 
 export default Home
