eaa1aa2a751634cd4e14080dddcc7b1f250b18ae
kotaro1574 <shake1574@gmail.com>
2022-11-13 13:56:20 +0900

    update: ページのstyle修正

--- src/pages/success/index.content.tsx
+++ src/pages/success/index.content.tsx
@@ -1,43 +1,9 @@
-import { useAuth0 } from '@auth0/auth0-react'
-import { Box, Button, Heading, Text } from '@chakra-ui/react'
-import { useEffect } from 'react'
+import { Box, Heading } from '@chakra-ui/react'
 
 export const PageContent = () => {
-  const {
-    isAuthenticated,
-    getAccessTokenSilently,
-    loginWithRedirect,
-    logout,
-    user,
-  } = useAuth0()
-  useEffect(() => {
-    const getAccessToken = async () => {
-      const accessToken = await getAccessTokenSilently()
-      console.log(accessToken)
-    }
-    getAccessToken()
-  }, [])
   return (
     <Box>
       <Heading>Success</Heading>
-      {isAuthenticated ? (
-        <Box>
-          <Text>{user?.name}</Text>
-          <Button onClick={() => logout()} colorScheme={'red'}>
-            ログアウトする
-          </Button>
-        </Box>
-      ) : (
-        <Box>
-          <Text>ログイン</Text>
-          <Button onClick={() => loginWithRedirect()} colorScheme={'blue'}>
-            ログインする
-          </Button>
-          <Button onClick={() => logout()} colorScheme={'red'}>
-            ログアウトする
-          </Button>
-        </Box>
-      )}
     </Box>
   )
 }
